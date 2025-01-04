f1fceb404d33a7fff0576706b53c2ef038a0226f
NidhaNureen <nnurwork@gmail.com>
2025-01-04 15:49:18 +1300

    ss deletion added

--- CleanUpFeatures.py
+++ CleanUpFeatures.py
@@ -1,25 +1,28 @@
 import os
 import time
 
-username = os.getlogin()
+
+def main_cleanup():
+    user = os.getlogin()
+    ss_directory = os.path.join(f"C:\\Users\\{user}\\Pictures\\Screenshots")
+    capture_directory = os.path.join(f"C:\\Users\\{user}\\Videos\\Captures")
+    if not os.path.exists(ss_directory):
+        ss_directory = os.path.join(f"C:\\Users\\{user}\\OneDrive\\Pictures\\Screenshots")
+    if not os.path.exists(ss_directory):
+        print("No screenshot directory found.")
+    # clear_screenshots(ss_directory)
+    clear_screenshots(capture_directory)
 
 
 # Function for clearing screenshots older than 30 days
-def clear_screenshots(user):
+def clear_screenshots(directory):
     try:
-        ss_directory = os.path.join(f"C:\\Users\\{user}\\Pictures\\Screenshots")
-        if not os.path.exists(ss_directory):
-            ss_directory = os.path.join(f"C:\\Users\\{user}\\OneDrive\\Pictures\\Screenshots")
-
-        if not os.path.exists(ss_directory):
-            print("No screenshot directory found.")
-            return
-
-        for filename in os.listdir(ss_directory):
-            file_path = os.path.join(ss_directory, filename)
+        for filename in os.listdir(directory):
+            file_path = os.path.join(directory, filename)
             if not os.path.isfile(file_path):
                 continue
-            if filename.endswith(".ini"):
+            if not filename.endswith(".png"):
+                print(f"Not a screenshot file: {filename}")
                 continue
             curr_time = time.time()
             file_age = curr_time - os.path.getctime(file_path)
@@ -33,5 +36,10 @@ def clear_screenshots(user):
         print(f"An error occured while trying to clear screenshots: {e}")
 
 
+# Function for clearing old vid files
 
-clear_screenshots(username)
+
+
+
+
+main_cleanup()
